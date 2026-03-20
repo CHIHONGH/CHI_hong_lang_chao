cfa11a991298cec2dc05479cc467546f63ef4c4f
BB86yszw <bsm541647@gmail.com>
2026-03-20 22:28:39 +0800

    Add files via upload

---
+++ common/00_临时common/RD_TFR_defines_changes.lua
@@ -0,0 +1,18 @@
+NDefines.NAir.AIR_NAVAL_KAMIKAZE_DAMAGE_MULT = 25 						--20.0 自杀式袭击伤害倍率 #机械胜过热血，孩子们！
+NDefines.NAir.AIR_NAVAL_KAMIKAZE_LOSSES_MULT = 10					 	--4.0 自杀式袭击损失倍率
+NDefines.NAir.BASE_KAMIKAZE_DAMAGE = 1 									--2.0 自杀式袭击最低伤亡
+NDefines.NAir.BASE_KAMIKAZE_TARGETING = 2 								--2.0 自杀式袭击重要目标权重
+--NDefines.NAir.AIR_WING_MAX_STATS_ATTACK = 10000                         --  空军最高攻击
+--NDefines.NAir.AIR_WING_MAX_STATS_DEFENCE = 10000                        --  空军最高防御
+--NDefines.NAir.AIR_WING_MAX_STATS_AGILITY = 10000                        --  空军最高机动
+--NDefines.NAir.AIR_WING_MAX_STATS_SPEED = 50000                         -- 800 最高速度 #最高速才800km/h,这河里吗？恒河里
+--NDefines.NAir.AIR_WING_MAX_STATS_BOMBING = 1000                         -- 100 最高轰炸伤害
+NDefines.NAir.BIGGEST_AGILITY_FACTOR_DIFF = 8                         	-- 4 高机动的伤害增益奖励上限
+NDefines.NAir.BIGGEST_SPEED_FACTOR_DIFF = 20                           	-- 3.5 高速度的伤害增益奖励上限
+--NDefines.NAir.COMBAT_BETTER_AGILITY_DAMAGE_REDUCTION = 0.85             -- 0.45 高机动的减伤奖励上限
+NDefines.NAir.COMBAT_BETTER_SPEED_DAMAGE_INCREASE = 0.99                -- 0.65 高速度的减伤奖励上限
+
+NDefines.NIntel.DYNAMIC_INTEL_SOURCE_EVENT_MAXIMUMS = { 100, 100, 100, 100 },
+NDefines.NIntel.DYNAMIC_INTEL_SOURCE_EVENT_ABSOLUTE_MAXIMUMS = { 150, 150, 150, 150 }
+
+NDefines.NBuildings.SAM_MISSION_SUPERIORITY = 50.0,      -- 5 地空导弹的空中优势
